cc2868274a27a79a94ba268aebed2c87c1f13b7a
Bogdan-101 <big_bogdan@rambler.ru>
2021-10-12 20:46:32 +0300

    feat: 1 lab

---
+++ lab1/SP.cpp
@@ -0,0 +1,170 @@
+// SP.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "SP.h"
+
+#define MAX_LOADSTRING 100
+
+#define IDM_START 1000
+#define IDM_STOP 1001
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+typedef struct _text {
+    int x;
+    int y;
+    LPTSTR str;
+} Text;
+
+Text textMessage;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_SP, szWindowClass, MAX_LOADSTRING);
+
+    !MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SP));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = (HICON)LoadImage(NULL, _T("./logo.ico"), IMAGE_ICON, 20, 20, LR_LOADFROMFILE);
+    wcex.hCursor        = (HICON)LoadImage(NULL, _T("./index.cur"), IMAGE_CURSOR, 20, 20, LR_LOADFROMFILE);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SP);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HMENU hMenu = CreateMenu();
+   AppendMenuW(hMenu, NULL, IDM_START, L"Start ride");
+   AppendMenuW(hMenu, NULL, IDM_STOP, L"Stop this mess");
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, hMenu, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    RECT rc;
+    PAINTSTRUCT ps;
+    switch (message)
+    {
+    case WM_CREATE:
+    {
+        GetClientRect(hWnd, &rc);
+        textMessage = { (rc.right - rc.left) / 2, (rc.bottom - rc.top) / 2, LPTSTR(L"Want to sell garage: +375785623415") };
+        break;
+    }
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // Parse the menu selections:
+        switch (wmId)
+        {
+        case IDM_START:
+            SetTimer(hWnd, 1, 50, NULL);
+            break;
+        case IDM_STOP:
+            KillTimer(hWnd, 1);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+        break;
+    }
+    case WM_TIMER:
+    {
+        GetClientRect(hWnd, &rc);
+        if (textMessage.x <= rc.left) {
+            textMessage.x = rc.right;
+        }
+        else {
+            textMessage.x -= 10;
+        }
+        InvalidateRect(hWnd, &rc, true);
+        break;
+    }
+    case WM_PAINT:
+    {
+        HDC hdc = BeginPaint(hWnd, &ps);
+        GetClientRect(hWnd, &rc);
+        TextOut(hdc, textMessage.x, textMessage.y, textMessage.str, _tcsclen(textMessage.str));
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
